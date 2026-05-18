d5660ed8a6925299bea995be7a26f2a1d396410f
Maksym Malyi <malyimaksym37@gmail.com>
2026-05-18 11:23:55 +0300

    mongoose configus setted

---
+++ common/constants.js
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  HOME: "/",
+  POSTS: "/posts",
+};
+
+export const STATUS_CODES = {
+  NOT_FOUND: 404,
+  BAD_REQUEST: 400,
+  SUCCESS: 200,
+};
+
+export const MESSAGES = {
+  NOT_FOUND: "404 not found",
+  BAD_REQUEST: "HTTP 400 Bad Request",
+};
